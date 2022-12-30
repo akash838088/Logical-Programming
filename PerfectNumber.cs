0872a14ae0efa504346a4838a24fe7f19f70817c
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2022-12-31 01:14:30 +0530

    check number

---
+++ PerfectNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+
+        public static void FindNumbers()
+        {
+
+            int n, i, sum;
+            int mn, mx;
+
+            Console.WriteLine("the perfect number within a given number of range");
+
+            Console.WriteLine("Input the starting range of number");
+
+            mn=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input the ending range of number");
+
+            mx=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Te pefect number within the given range");
+
+            for(n=mn; n<=mx; n++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + 1;
+                    i++;
+                }
+                if (sum == n)
+                    Console.Write("{0}", n);
+            }
+        }
+    }
+}
